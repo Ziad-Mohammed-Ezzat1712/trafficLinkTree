3d64785df5996e7119b5c19853a8819cc478e6e2
Ziad-Mohammed-Ezzat1712 <ziadmohammed1712@gmail.com>
2026-04-01 18:08:24 +0200

    finsh

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Home from './Components/home/home';
+
 import './App.css';
+import Home from './Components/Home/Home';
 let x = createBrowserRouter([
 {index : true,element:<Home/> },
 
